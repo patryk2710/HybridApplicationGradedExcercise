10dbe32b1a4883f276515d167db681e4057baf88
sean-mcgregor <worksmcg@gmail.com>
2021-02-28 15:32:01 +0200

    getAll FINALLY FUCKING WORKS

--- components/homeView.js
+++ components/homeView.js
@@ -1,118 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import TopBarHome from './topBarHome'
 import AdList from './AdList'
-import axios from 'axios'
-import adImage from '../assets/eMeme.jpg'
 
-//const allAds = axios.get('https://webinterfaces-api-patrykr.herokuapp.com/postings/search')
-//console.log(allAds)
-const allAds = [
-  {
-    id: 1,
-    title: "Valittu Kansa",
-    username: "Antti Tuisku",
-    images: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-  },
-  {
-    id: 2,
-    title: "Noise",
-    username: "Nightwish",
-    images: "https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png"
-  },
-  {
-    id: 3,
-    title: "Intentions",
-    username: "Justin Bieber",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 4,
-    title: "Majakka",
-    username: "Juno",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 21,
-    title: "Valittu Kansa",
-    username: "Antti Tuisku",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 22,
-    title: "Noise",
-    username: "Nightwish",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 23,
-    title: "Intentions",
-    username: "Justin Bieber",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 24,
-    title: "Majakka",
-    username: "Juno",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 31,
-    title: "Valittu Kansa",
-    username: "Antti Tuisku",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 32,
-    title: "Noise",
-    username: "Nightwish",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 33,
-    title: "Intentions",
-    username: "Justin Bieber",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-  {
-    id: 34,
-    title: "Majakka",
-    username: "Juno",
-    images: "https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg"
-  },
-];
+export default homeView = () => {
 
-let apiAds = {}
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
 
-const request = new XMLHttpRequest()
-request.open('get', 'https://webinterfaces-api-patrykr.herokuapp.com/postings/search')
-request.send()
-request.onload = () => {
+  useEffect(() => {
+    fetch('https://webinterfaces-api-patrykr.herokuapp.com/postings/search')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
-  if (request.status === 200){
-
-    apiAds = JSON.parse( request.response)
-  } else {
-
-    console.log('error')
-  }
-}
-
-console.log(apiAds)
-console.log(allAds)
-
-const homeView = () => {
   return (
-    //<View style={{flex:1, flexDirection: 'column', height: '100%'}}>
-      <ScrollView>
-        <View style={styles.backgroundTop}>
-          <TopBarHome style={{flex: 1}}></TopBarHome>
-        </View>
-        <AdList
-          ads = { apiAds }>
-        </AdList>
-      </ScrollView>
-    //</View>
+    <ScrollView>
+      <View style={styles.backgroundTop}>
+        <TopBarHome style={{flex: 1}}></TopBarHome>
+      </View>
+      <AdList
+        ads = { data }>
+      </AdList>
+    </ScrollView>
   )
 }
 
@@ -134,5 +47,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 })
-
-export default homeView
